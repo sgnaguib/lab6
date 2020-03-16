e950631bc422550b8b66cf43cc29c555025fbc47
Saeed <sgnaguib@gmail.com>
2020-03-15 17:08:06 -0700

    db updated

--- db/db.js
+++ db/db.js
@@ -1,10 +1,10 @@
 const Pool = require('pg').Pool;
 
 const pool = new Pool({  
-    host: 'localhost',  
-    user: 'postgres',  
-    database: 'lab6',  
-    password: '9312',
+    host: 'ec2-3-213-192-58.compute-1.amazonaws.com',  
+    user: 'qefpwiuicticzb',  
+    database: 'dfesdncqjsd6p3',  
+    password: '3a4fcab43d6c9f95cacb6910edac6ea42927a34c91a23240bc961ecb8531c5b2',
     port: 5432,
     ssl: false
 });  
